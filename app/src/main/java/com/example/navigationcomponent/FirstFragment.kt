ef5c8464a29f38d76c401b98c3c9114d48c3d2a6
Admin <cholongxu10@gmail.com>
2023-01-05 22:56:49 +0700

    git remote add origin https://github.com/tu4nh/NavigationComponent.git

---
+++ app/src/main/java/com/example/navigationcomponent/FirstFragment.kt
@@ -0,0 +1,57 @@
+package com.example.navigationcomponent
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.example.navigationcomponent.databinding.FragmentFirstBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.btnOpenFragment2.setOnClickListener {
+            findNavController().navigate(R.id.secondFragment)
+        }
+
+        binding.btnOpenFragment3.setOnClickListener {
+            findNavController().navigate(R.id.thirdFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
